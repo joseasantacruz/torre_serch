6ef0fb6094a31e2db703bdba18bb5e2150dc9209
Jose Santacruz <joseasantacruz@gmail.com>
2021-03-18 21:49:27 -0300

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import './App.css';
+function Footer() {
+  return (
+      <footer className="App-footer">
+        <p>
+          Web site to search jobs and people in <a className="App-link" href="https://torre.co" target="_blank" >Torre.co</a> by Jose Santacruz - joseasantacruz@gmail.com
+        </p>
+      </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import './App.css';
+function Header() {
+  return (
+    <header className="App-header">
+        Torre Search
+    </header>
+  );
+}
+
+export default Header;
